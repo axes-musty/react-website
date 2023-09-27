261fbcddff5a4d4bfb4ed6cb97ea76906ee7e902
Axes Musty <>
2023-09-27 11:57:32 +0200

    Homepage Update

--- src/componenets/HeroSection.js
+++ src/componenets/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css'
 function HeroSection () {
   return (
     <div className='hero-container'>
-        <video src='/videos/video-1.webm' autoPlay muted loop/>
+        <video src='/videos/video-1.mp4' autoPlay muted loop/>
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
